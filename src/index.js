02a2c7d1b4b3a45f44ae9c4830c7782d8f942d48
Luis H. Bajj Jr <luqven@gmail.com>
2019-05-14 12:05:58 -0400

    create basic skeleton of game

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("webpack is running...");
   console.log("DOM fully loaded and parsed");
   window.requestAnimFrame = Util.requestAnimFrame;
-
+  // create the game canvas
   const canvas = new Canvas({
     canvas: document.getElementById("gameCanvas"),
     container: document.getElementById("gameContainer")
   });
+  // create the game board
+  // create the player
 
   const draw = () => {
     window.requestAnimationFrame(draw);
+    // handle game logic
     canvas.render();
   };
 
