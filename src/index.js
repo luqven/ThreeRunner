784187c4374aef5723c9e0c0d66dca9e65027d3a
Luis H. Bajj Jr <luqven@gmail.com>
2019-05-29 09:41:56 -0400

    add basic button controls

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("webpack is running...");
   console.log("DOM fully loaded and parsed");
   window.requestAnimFrame = Util.requestAnimFrame;
+  // style the page's buttons
+  let controlsBtn = document.querySelector("#controls-btn");
+  controlsBtn.addEventListener("click", e => {
+    let controls = document.querySelector(".controlsModal");
+    controls.classList.toggle("transparent");
+  });
+  let volumeBtn = document.querySelector("#volume-btn");
+  volumeBtn.addEventListener("click", e => {
+    let innerText = volumeBtn.innerHTML;
+    if (innerText === "Mute") {
+      volumeBtn.innerHTML = "Unmute";
+    } else {
+      volumeBtn.innerHTML = "Mute";
+    }
+  });
   // create the game canvas
   const canvas = new Canvas({
     canvas: document.getElementById("gameCanvas"),
